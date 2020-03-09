777ceff59e36137a9011f75de795781c716b9a47
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-09 23:04:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,14 +31,15 @@ end
 	end
 
   post "/login" do
-     if @user && @user.authenticate(params[:password])
-       session[:user_id] = @user.id
-
-       redirect to "/account"
-     else
-       redirect to "/failure"
-     end
-  end	
+    @user = User.find_by(:username => params[:username])
+    
+     if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/success"
+  else
+    redirect "/failure"
+  end
+end
 
 
 	get "/success" do
